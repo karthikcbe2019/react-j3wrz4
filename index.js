d93cb5514acc0858f40bb57c9d552800c97e1176
karthik <karthikcbe2019@gmail.com>
2019-08-21 16:44:06 +0530

    #1 issue solved

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ class App extends Component {
   render() {
     return (
       <div>
+
+        
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
